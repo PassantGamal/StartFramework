d923012b73b7d685cf8f7913e5dd2687a9250427
PassantGamal <basantgamal070@gmail.com>
2025-01-15 09:04:45 +0200

    Initial commit of Angular project

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,16 @@
+
 import { Component } from '@angular/core';
+import{FormsModule}from'@angular/forms'
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  nameValue: string = '';
+  ageValue: string = '';
+  emailValue:string='';
+  passwordValue:string=''
 }
